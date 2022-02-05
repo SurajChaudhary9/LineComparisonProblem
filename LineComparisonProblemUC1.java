443955a24732a3eac6c24db7c34518e743d15834
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-05 19:32:00 +0530

    UC2 Added : Compare two lines are Equal

--- LineComparisonProblemUC1.java
+++ LineComparisonProblemUC1.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class LineComparisonProblemUC1 {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-        double x1,x2,y1,y2;
-        int distance;
+		Scanner sc = new Scanner(System.in);
+        double x1,x2,y1,y2,x3,y3,x4,y4;
+        int line1,line2;
         
+        //For first line
         System.out.println("Enter x1 & y1 values of First point");
         x1=sc.nextDouble();
         y1=sc.nextDouble();
@@ -17,8 +18,30 @@ public class LineComparisonProblemUC1 {
         x2=sc.nextDouble();
         y2=sc.nextDouble();
         
-        distance=(int)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("Distance between two points = "+distance);
+        line1=(int)Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("Distance of First line = "+line1);
+        
+        
+        //For second line 
+        System.out.println("Enter x3 & y3 values of Third point");
+        x3=sc.nextDouble();
+        y3=sc.nextDouble();
+        
+        System.out.println("Enter x4 & y4 values of Fourth point");
+        x4=sc.nextDouble();
+        y4=sc.nextDouble();
+        
+        line2=(int)Math.sqrt((x4 - x3) * (x4 - x3) + (y4 - y3) * (y4 - y3));
+        System.out.println("Distance of Second line = "+line2);
+        
+        String str1 = Double.toString(line1);
+        String str2 = Double.toString(line2);
+        
+        //checking for line equality
+        if(str1.equals(str2)==true)
+        	System.out.println("Both lines are Equal");
+        else
+        	System.out.println("Both The Lines are Not Equal");
 
 	}
 
